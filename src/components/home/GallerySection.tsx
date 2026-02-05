a7924fef7a7b2af32d72ba0ebe4e7fdcdbf3b9fb
muhammed-programmer <muhammed@smsglobe.net>
2026-02-05 17:47:26 +0100

    update gallery

--- src/components/home/GallerySection.tsx
+++ src/components/home/GallerySection.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { useState } from "react";
 import cloudinaryMap from "@/data/cloudinaryMap.json";
 import { ChevronLeft, ChevronRight, X, Play } from "lucide-react";
